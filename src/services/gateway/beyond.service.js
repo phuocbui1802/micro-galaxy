c7c79981d1748fb1d6ed10255ea4fff138a3820d
as-nam <nam.tran@audienceservc.com>
2020-02-27 16:31:40 +0700

    Fixed get user in mutiple data source

--- src/services/gateway/beyond.service.js
+++ src/services/gateway/beyond.service.js
@@ -70,12 +70,12 @@ module.exports = {
 
     async handleGetBeyondUser (ctx) {
       const unit = ctx.params.unit || 'default';
-      const response = await this.getUserByEmail(ctx.params.email_address, unit);
+      const recipients = await this.getUserByEmail(ctx.params.email_address, unit);
 
       return {
-        total: response.data.recipients.length,
+        total: recipients.length,
         message: STATUS.OK,
-        recipients: response.data.recipients
+        recipients
       };
     },
 
@@ -258,19 +258,36 @@ module.exports = {
       return errors;
     },
 
-    async getUserByEmail (email, unit) {
-      this.logger.info('Getting: ', email, ' from beyond');
-      const url = `${process.env.BEYOND_HOST}${ENDPOINTS.GET_USER}`;
-      const sessionId = await this.getValidBeyondCredential(unit);
+    async getDataSources (sessionId) {
+      const dataSourceUrl = `${process.env.BEYOND_HOST}${ENDPOINTS.GET_DATASOURCE}`;
+      const { data } = await axios.post(dataSourceUrl, { session_id: sessionId });
+      return data.datasources;
+    },
 
-      return axios({
-        method: 'post',
-        url: url,
-        data: {
+    async getUserByEmail (email, unit) {
+      const sessionId = await this.getValidBeyondCredential(unit);
+      const dataSources = await this.getDataSources(sessionId);
+      const getUserUrl = `${process.env.BEYOND_HOST}${ENDPOINTS.GET_USER}`;
+      let recipients = [];
+
+      for (const dataSource of dataSources) {
+        const { data: recipientData } = await axios.post(getUserUrl, {
           session_id: sessionId,
-          emails: [email]
+          emails: [email],
+          source_id: dataSource.source_id,
+          allow_duplicates: true
+        });
+
+        if (recipientData.recipients.length) {
+          const temp = recipientData.recipients.map(recipient => {
+            recipient.source_id = dataSource.source_id;
+            return recipient;
+          });
+          recipients = recipients.concat(temp);
         }
-      });
+      }
+
+      return recipients;
     },
 
     createEmailObject (email) {
@@ -350,12 +367,10 @@ module.exports = {
     async unsubscribeEmail (email, unit) {
       const sessionId = await this.getValidBeyondCredential(unit);
 
-      const dataSourceUrl = `${process.env.BEYOND_HOST}${ENDPOINTS.GET_DATASOURCE}`;
-
-      const { data } = await axios.post(dataSourceUrl, { session_id: sessionId });
+      const dataSources = await this.getDataSources(sessionId);
       let errors = [];
 
-      for (const dataSource of data.datasources) {
+      for (const dataSource of dataSources) {
         const getUserUrl = `${process.env.BEYOND_HOST}${ENDPOINTS.GET_USER}`;
 
         const { data: recipientData } = await axios.post(getUserUrl, {
